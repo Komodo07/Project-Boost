4115460cfba6b8c2354d2ba61f88ab067c78367b
Jason Tucker <komodo.07@outlook.com>
2024-10-28 23:06:50 -0500

    Created input for rotation
    Created input to rotate the rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,20 +1,42 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustSpeed;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
+    }    
+
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
     }
 
-    private void Update()
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log($"Rotation at {rotationInput}");
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Thrusting to infinity and beyond!");
-        }        
+            rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.fixedDeltaTime);
+        }
     }
 }
