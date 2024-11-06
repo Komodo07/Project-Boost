55702198f1223795ea3d2610ee45eb5c5d69dbf6
Komodo07 <komodo.07@outlook.com>
2024-11-06 16:44:15 -0600

    Added more scenes
    Added more scenes for player to progress through. Edited Collision script to progress player when hitting objects with the 'Finish' tag.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,16 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    int currentSceneIndex;
+
+    private void Awake()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -11,17 +20,42 @@ public class CollisionHandler : MonoBehaviour
                 break;
             
             case "Finish":            
-                Debug.Log("Collided into Finish object");
-                break;
+                int nextSceneIndex = currentSceneIndex + 1;
+                if(nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+                {
+                    LoadNextScene(nextSceneIndex);
+                    break;
+                }
+                else
+                {
+                    GameOver();
+                    break;
+                }
             
             case "Fuel":            
                 Debug.Log("Collided into Fuel object");
                 break;
             
-            default:            
-                Debug.Log("Player destroyed");
+            default:
+                ReloadScene();                
                 break;
             
         }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("Congratulations!! You have finished the game!");
+        //TODO: Create and event that can be subcribed to by the Movement script
+    }
+
+    private void LoadNextScene(int nextSceneIndex)
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
