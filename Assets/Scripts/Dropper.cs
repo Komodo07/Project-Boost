bf8b59c198474011066067a6767b31a5120e04f0
Komodo07 <komodo.07@outlook.com>
2025-01-21 16:13:35 -0600

    Worked on Dropper Object
    Defined methods in Dropper script to enable the animator and gravity properties. Methods will be called by a trigger object when the player collides with it.

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    Rigidbody rb;
+    Animator shake;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        shake = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAnimation()
     {
-        
+        shake.enabled = true;
     }
+
+    public void EnableGravity()
+    {
+        rb.useGravity = true;
+    }
+    
 }
